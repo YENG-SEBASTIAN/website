e6ec19760d03804956bb2fbd13458ed4ef082664
YENG-SEBASTIAN <yengsebastian1@gmail.com>
2024-09-18 06:43:59 +0200

    hero section and index file

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,80 +1,43 @@
 import React from 'react';
-import Slider from 'react-slick';
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import { FaFileAlt, FaWhatsapp } from 'react-icons/fa';
 import bg1 from '../assets/images/bg_1.png';
-import bg2 from '../assets/images/bg_2.png';
+import cv from '../assets/files/SEBASTIAN_YENG.pdf';
 
 const HeroSection = () => {
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 5000,
-    arrows: false,
-    responsive: [
-      {
-        breakpoint: 768,
-        settings: {
-          dots: false,
-        }
-      }
-    ]
-  };
-
   return (
     <section id="home-section" className="hero bg-gray-800 text-white min-h-screen relative">
-      <Slider {...settings}>
-        {/* First Slide */}
-        <div className="slider-item h-screen">
-          <div className="container mx-auto h-full flex flex-col-reverse md:flex-row items-center justify-center">
-            <div className="md:w-1/2 w-full flex items-center justify-center md:justify-start h-full px-5 md:px-10 mt-10 md:mt-0 md:ml-16">
-              <div className="text-left">
-                <span className="subheading text-gray-400 block">Hello!</span>
-                <h1 className="mb-4 mt-3 text-4xl md:text-5xl font-bold">I'm <span className="text-blue-500">Yeng Sebastian</span></h1>
-                <h2 className="mb-4 text-2xl md:text-3xl font-semibold">A Software Engineer</h2>
-                <div className="flex space-x-4 mt-5">
-                  <a href="#contact-section" className="btn btn-primary bg-blue-600 text-white py-3 px-6 rounded-full">Hire me</a>
-                  <a href='#projects-section' className="btn btn-outline border-2 border-white text-white py-3 px-6 rounded-full">My works</a>
-                </div>
+      <div className="slider-item h-screen">
+        <div className="container mx-auto h-full flex flex-col-reverse md:flex-row items-center justify-center">
+          <div className="md:w-1/2 w-full flex items-center justify-center md:justify-start h-full px-5 md:px-10 mt-10 md:mt-0 md:ml-16">
+            <div className="text-left">
+              <span className="subheading text-gray-400 block">Hello!</span>
+              <h1 className="mb-4 mt-3 text-4xl md:text-5xl font-bold">I'm <span className="text-blue-500">Yeng Sebastian</span></h1>
+              <h5 className="mb-4 text-2xl md:text-3xl font-semibold">Software Engineer</h5>
+              <p>
+                With a knack for innovation and an eye for detail, I specialize in building software 
+                solutions that transform ideas into reality. My passion lies in solving challenges and 
+                delivering user-centric experiences that drive success. Always staying ahead of the curve, 
+                I’m committed to pushing the limits of technology to create a better future.
+              </p>
+              <div className="flex space-x-4 mt-5">
+                <a href={cv} download className="btn btn-primary bg-blue-600 text-white py-3 px-6 rounded-full flex items-center">
+                  <FaFileAlt className="mr-2" /> Download CV
+                </a>
+                <a href="https://wa.me/233547337110" target='_blank' className="btn btn-outline border-2 border-white text-white py-3 px-6 rounded-full flex items-center">
+                  <FaWhatsapp className="mr-2" /> Let's talk
+                </a>
               </div>
             </div>
-            <div className="md:w-1/2 w-full flex justify-center h-full">
-              <img 
-                src={bg1} 
-                alt="Yeng Sebastian" 
-                className="h-auto w-full max-w-md object-contain rounded-lg shadow-lg"
-              />
-            </div>
+          </div>
+          <div className="md:w-1/2 w-full flex justify-center h-full">
+            <img 
+              src={bg1} 
+              alt="Yeng Sebastian" 
+              className="h-auto w-full max-w-md object-contain rounded-lg shadow-lg"
+            />
           </div>
         </div>
-
-        {/* Second Slide */}
-        <div className="slider-item h-screen">
-          <div className="container mx-auto h-full flex flex-col-reverse md:flex-row items-center justify-center">
-            <div className="md:w-1/2 w-full flex items-center justify-center md:justify-start h-full px-5 md:px-10 mt-10 md:mt-0 md:ml-16">
-              <div className="text-left">
-                <span className="subheading text-gray-400 block">Hello!</span>
-                <h1 className="mb-4 mt-3 text-4xl md:text-5xl font-bold">I'm a <span className="text-blue-500">Fullstack Developer</span> based in Ghana</h1>
-                <div className="flex space-x-4 mt-5">
-                  <a href='#contact-section' className="btn btn-primary bg-blue-600 text-white py-3 px-6 rounded-full">Hire me</a>
-                  <a href='#projects-section' className="btn btn-outline border-2 border-white text-white py-3 px-6 rounded-full">My works</a>
-                </div>
-              </div>
-            </div>
-            <div className="md:w-1/2 w-full flex justify-center h-full">
-              <img 
-                src={bg2} 
-                alt="Fullstack Developer" 
-                className="h-auto w-full max-w-md object-contain rounded-lg shadow-lg"
-              />
-            </div>
-          </div>
-        </div>
-      </Slider>
+      </div>
     </section>
   );
 };
